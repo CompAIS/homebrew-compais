aa14d8f72edde7045ca57ec430ae6affcee34c5a
marxide <andrew@151-lease-15.scem.uws.edu.au>
2014-12-18 19:09:22 +1100

    Fixed karma path problem
    To reinstall:
    $ brew remove karma && rm $(brew --prefix)/karma
    Close and open new a Terminal to reset your PATH.
    $ brew install karma
    New Terminal windows should now work as expected.

--- karma.rb
+++ karma.rb
@@ -1,26 +1,26 @@
 require "formula"
 
 class Karma < Formula
-  homepage "http://www.atnf.csiro.au/computing/software/karma/"
-  url "ftp://ftp.atnf.csiro.au/pub/software/karma/karma-1.7.20-x86_64_Darwin-11.2.tar.bz2"
-  sha1 "e78aac1214cb9dcd7887f3fa19a279d05d3c5f21"
+  homepage 'http://www.atnf.csiro.au/computing/software/karma/'
+  url 'ftp://ftp.atnf.csiro.au/pub/software/karma/karma-1.7.20-common.tar.bz2'
+  sha1 'f549ab0c0103800e6883c71d1c2cecefce242e45'
   
-  resource 'common' do
-    url 'ftp://ftp.atnf.csiro.au/pub/software/karma/karma-1.7.20-common.tar.bz2'
-    sha1 'f549ab0c0103800e6883c71d1c2cecefce242e45'
+  resource 'darwin' do
+    url 'ftp://ftp.atnf.csiro.au/pub/software/karma/karma-1.7.20-x86_64_Darwin-11.2.tar.bz2'
+    sha1 'e78aac1214cb9dcd7887f3fa19a279d05d3c5f21'
   end  
-
+  
   depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  patch :DATA
 
   def install
      ENV.deparallelize  # if your formula fails when building in parallel
 
-     # download karma-common to the temporary buildpath
-     resource("common").stage { buildpath.install Dir.glob("*", File::FNM_DOTMATCH) - %w[. ..] }
+     # download karma-darwin to the temporary buildpath
+     resource("darwin").stage { buildpath.install Dir.glob("*", File::FNM_DOTMATCH) - %w[. ..] }
      prefix.install buildpath # copy entire buildpath to the cellar
-
-     # bin dist requires this symlink. No way around it unless we compile from source.
-     ln_s "#{prefix}/karma-1.7.20/x86_64_Darwin-11.2", "/usr/local/karma"
+     ln_s "#{prefix}/karma-1.7.20/x86_64_Darwin-11.2", HOMEBREW_PREFIX/"karma"
   end
 
   def caveats
@@ -39,3 +39,17 @@ class Karma < Formula
     s
   end
 end
+__END__
+diff --git a/.karmarc b/.karmarc
+index c25d8a0..1969045 100644
+--- a/.karmarc
++++ b/.karmarc
+@@ -98,7 +98,7 @@ else
+     fi
+ fi
+ 
+-PATH=${KARMABASE}/cm_script:${KARMABASE}/csh_script:$KARMABASE/site/$MACHINE_OS/bin:$KARMABINPATH:$PATH
++PATH=$PATH:${KARMABASE}/cm_script:${KARMABASE}/csh_script:$KARMABASE/site/$MACHINE_OS/bin:$KARMABINPATH
+ export PATH
+ 
+ unset i
